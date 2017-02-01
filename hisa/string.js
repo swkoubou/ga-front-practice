7b6b309972450f6fcfbc4ac20dc8e8de9e58cfb9
hisatsugu <syou.0621s@gmail.com>
2017-02-01 12:22:33 +0900

    uho

--- hisa/string.js
+++
@@ -1,9 +0,0 @@
-var str='にわにはにわにわにわとりがいる';
-console.log(str.indexOf('にわ'));
-console.log(str.lastIndexOf('にわ'));
-console.log(str.indexOf('にわ',3));
-console.log(str.lastIndexOf('わ',5));
-console.log(str.indexOf('ガーデン'));
-
-
-console.log(str);
