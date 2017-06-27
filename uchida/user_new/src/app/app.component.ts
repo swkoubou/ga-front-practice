d74248848dc67e5d2012ed6e2947df5934e676cd
AK <akgalaxy@akgalaxy.local>
2017-06-27 22:10:19 +0900

    内田君の場所、画像ファイルのアップロード実装

--- uchida/user_new/src/app/app.component.ts
+++ uchida/user_new/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  fa:string;
+
+  constructor(private element: ElementRef) {}
+
+  uho(event):void {
+    var Data:File = event.target.files[0];
+    if(Data.type.indexOf("image") < 0){
+      alert("画像以外渡すなクソ");
+      return;
+    }
+
+    var image = this.element.nativeElement.querySelector('.image');
+    var reader = new FileReader();
+    reader.onload = function() {
+        var src = reader.result;
+        image.src = src;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+
+
 }
